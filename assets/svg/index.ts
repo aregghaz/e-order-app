e5f931ba9144982c38254478bed92f0b5ee72b7f
TAS <tikotas@mail.ru>
2023-07-16 14:58:05 +0400

    added svg component

---
+++ assets/svg/index.ts
@@ -0,0 +1,3 @@
+import NoImage from './no-image.svg'
+
+export { NoImage }
--- src/navigation/PhoneRegisterStack.tsx
+++ src/navigation/PhoneRegisterStack.tsx
@@ -7,12 +7,13 @@ import { StyleSheet, View } from 'react-native'
 import PhoneInput from 'react-native-phone-input'
 
 import { SHOP_API } from '~api'
+// import { ImgOrSvg } from '~components/ImgOrSvg'
 import { CustomButton } from '~components/molecules/CustomButton'
 import { SCREEN } from '~constants'
-
+// import { NoImage } from 'assets/svg'
 export const PhoneRegisterStack: FC = () => {
   const [phoneNumber, setPhoneNumber] = useState('')
-  const navigation = useNavigation()
+  const navigation = useNavigation<any>()
 
   const handlePhoneNumberChange = (number: string) => {
     setPhoneNumber(number)
@@ -41,6 +42,8 @@ export const PhoneRegisterStack: FC = () => {
       {/*<TouchableOpacity onPress={navigation.navigate(SCREEN.STACK_SIGN_IN)}>*/}
       {/*  <Text>Already have account ?</Text>*/}
       {/*</TouchableOpacity>*/}
+      {/*<ImgOrSvg />*/}
+      {/*<NoImage height={200} width={200} fill={'red'} />*/}
     </View>
   )
 }
