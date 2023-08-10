b413aaa97d5c6d10cc268bcfb690c30e332cfb1f
TAS <tikotas@mail.ru>
2023-08-11 01:28:22 +0400

    getting shop id | add to cart start to work | shop cart indicator is behind one step..

--- src/components/SubList.tsx
+++ src/components/SubList.tsx
@@ -27,12 +27,12 @@ export const SubList: FC<IProps> = ({
 }) => {
   const handlePressCheck = async (title: string, group: string, shop_id: string) => {
     if (group === 'shop') {
-      navigation.navigate(SCREEN.STACK_MAIN_TAB, { title })
+      // navigation.navigate(SCREEN.STACK_MAIN_TAB, { title })
       handlePress()
       await setShopId(shop_id)
       // navigation.navigate(SCREEN.DRAWER_MENU, { title })
       console.log(shop_id, 'PPPPPPP')
-      // navigation.closeDrawer()
+      navigation.closeDrawer()
     } else {
       handlePress()
       !hasChildren ? navigation.navigate(findScreenName, { title }) : null
